dfcc6c79ab73d1df60b131ac411a10ed4ba1d451
JVelezFD <jvelez117@gmail.com>
2022-07-12 10:15:50 -0400

    Added the layout of the ReadMe file.

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -34,11 +34,50 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!data.license){
+    '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+
+  ## Description 
+  ${data.description}
+
+  ## Table of Contents
+  -[Description](#description)
+  -[Installation](#installation)
+  -[Usage](#usage)
+  -[Contributing](#contributing)
+  -[Tests](#tests)
+  -[Credits](#credits)
+  -[Features](#features)
+  -[License](#license)
+
+  ##Installation
+  ${data.installation}
+
+  ##Usage
+  ${data.usage}
+
+  ##Contributing
+  ${data.contributing}
+
+  ##Tests
+  ${data.tests}
+
+  ##Credits
+  ${data.credits}
+
+  ##Features
+  ${data.features}
+
+  ##License
+  ${data.license}
 
 `;
 }
