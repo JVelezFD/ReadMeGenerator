a6fab6998b6915ff303269e8be0a44c5c58e9b31
JVelezFD <jvelez117@gmail.com>
2022-07-12 10:04:14 -0400

    installed INQ, and added Markdown code for data and license rendering

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,10 +1,36 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (data.license === 'MIT'){
+    data.licenseBadge = 'MIT';
+  }
+  if (data.license === 'Community License'){
+    data.licenseBadge = 'Community License';
+  }
+  if (data.license === 'GNU GPLv3'){
+    data.licenseBadge = 'GNU GPLv3';
+  }
+  else {
+    '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (data.license === 'MIT'){
+    data.licenseLink = 'MIT';
+  }
+  if (data.license === 'Community License'){
+    data.licenseLink = 'Community License';
+  }
+  if (data.license === 'GNU GPLv3'){
+    data.licenseLink = 'GNU GPLv3';
+  }
+  else{
+    '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
